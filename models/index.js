9b14b6121874c3e30da95c03766456104b1082c3
Flunder620 <jguzman12310@yahoo.com>
2023-10-26 23:48:52 -0500

    updated index.js

--- models/index.js
+++ models/index.js
@@ -1,2 +1,16 @@
 // importing models
 const Character = require('./Character')
+const Class = require('./Class')
+const Race = require('./Race')
+const Skill = require('./Skill')
+const Armor = require('./Armor')
+const User = require('./User')
+
+module.exports = {
+    Character,
+    Class,
+    Race,
+    Skill,
+    Armor,
+    User,
+  };
