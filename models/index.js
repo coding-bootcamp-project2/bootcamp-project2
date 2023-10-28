71a266428ac7a2187e6a02243cb1a69aedcf4bcb
Flunder620 <jguzman12310@yahoo.com>
2023-10-28 11:18:55 -0500

    added relationships

--- models/index.js
+++ models/index.js
@@ -15,7 +15,49 @@ Character.belongsTo(User, {
     foreignKey: 'user_id'
 })
 User.hasMany(Character, {
+    foreignKey: 'user_id'
+})
+Class.belongsTo(Character, {
     
+})
+Character.hasOne(Class, {
+
+})
+Race.belongsTo(Character, {
+
+})
+Character.hasOne(Race, {
+
+})
+Skill.belongsTo(Character, {
+
+})
+Character.hasOne(Skill, {
+
+})
+Armor.belongsTo(Character, {
+
+})
+Character.hasOne(Armor, {
+
+})
+Character.hasMany(CharacterSpell, {
+
+})
+CharacterSpell.hasMany(Spell, {
+
+})
+Spell.belongsTo(CharacterSpell, {
+
+})
+Character.hasMany(CharacterWeapon, {
+
+})
+CharacterWeapon.hasMany(Weapon, {
+
+})
+Weapon.belongsTo(CharacterWeapon, {
+
 })
 // Exporting
 module.exports = {
