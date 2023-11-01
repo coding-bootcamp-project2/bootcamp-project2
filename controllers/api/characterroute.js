64c3aff9f54fcbdbeaca284a430cb8b987e92a14
Flunder620 <jguzman12310@yahoo.com>
2023-11-01 18:53:22 -0500

    set up routes

--- controllers/api/characterroute.js
+++ controllers/api/characterroute.js
@@ -1,2 +1,16 @@
 const router = require("express").Router();
+const { Character } = require('../../models');
+
+router.get('/', async(req, res)=> {
+
+})
+
+router.put('/', async(req, res)=> {
+
+})
+
+router.post('/', async(req, res)=> {
+
+})
+
 module.exports = router;
