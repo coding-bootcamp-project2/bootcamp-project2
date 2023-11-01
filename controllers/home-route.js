b2b42ac2d85c0f3a9f06150bf9b1220ab71d56ee
ChgDave <chgdave@gmail.com>
2023-10-31 21:54:46 -0500

    added homepage feature

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -2,15 +2,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   try {
-    const character = {
-      id: 1,
-      user_id: 1,
-      class_id: 1,
-      level: 10,
-      hit_points: 100,
-      createdAt: "10/30/2023",
-    };
-    res.render("character", { character, loggedIn: req.session.loggedIn });
+    res.render("homepage");
   } catch (error) {
     res.status(500).json(error);
   }
