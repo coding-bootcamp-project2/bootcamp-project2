d1469947c131a5bf64806c3cdc9fcea4069db07e
Flunder620 <jguzman12310@yahoo.com>
2023-11-01 19:20:59 -0500

    added put, post, and delete routes

--- controllers/api/characterroute.js
+++ controllers/api/characterroute.js
@@ -1,16 +1,51 @@
 const router = require("express").Router();
-const { Character } = require('../../models');
+const { Character } = require("../../models");
 
-router.get('/', async(req, res)=> {
+router.get("/", async (req, res) => {});
 
-})
+router.post("/", async (req, res) => {
+  try {
+    const newCharacter = await Character.create({
+      ...req.body,
+    });
+    res.json(newCharacter);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.put('/', async(req, res)=> {
+router.put("/:id", async (req, res) => {
+  try {
+    const [affectedRows] = await Character.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-})
+router.delete("/:id", async (req, res) => {
+  try {
+    const [affectedRows] = Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-router.post('/', async(req, res)=> {
-
-})
-
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
