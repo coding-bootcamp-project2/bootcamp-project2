0d5be57f2498fd7545988538d7008ee022f01171
ChgDave <chgdave@gmail.com>
2023-11-01 19:06:56 -0500

    fixed loggedIn data on main page

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   try {
-    res.render("homepage");
+    res.render("homepage", { loggedIn: req.session.loggedIn });
   } catch (error) {
     res.status(500).json(error);
   }
@@ -13,6 +13,7 @@ router.get("/login", async (req, res) => {
   try {
     // if it's already logged in, just render the homepage
     if (req.session.loggedIn) {
+      console.log(req.session.loggedIn);
       res.redirect("/");
       return;
     }
