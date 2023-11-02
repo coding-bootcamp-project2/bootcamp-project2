57a93fb11f924aecae903d86e608064e009a5e53
Flunder620 <jguzman12310@yahoo.com>
2023-11-01 20:59:34 -0500

    fixed seed

--- seeds/character.js
+++ seeds/character.js
@@ -3,7 +3,7 @@ const { Character } = require('../models'); // placeholder route
 const characterData = [
     {
         character_name: 'testname',
-        user_id: 5,
+        user_id: 1,
         class_id: 1,
         race_id: 2,
         level: 1,
