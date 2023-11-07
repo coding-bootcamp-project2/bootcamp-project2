5ed428060744c2d0c0cef72c97264c0900c8752e
Flunder620 <jguzman12310@yahoo.com>
2023-11-06 22:49:15 -0600

    updated character js

--- public/js/character.js
+++ public/js/character.js
@@ -1,1 +1,18 @@
-const updateCharacter = document.querySelector("")
+const characterTemplate = document.querySelector("character-template")
+const characterNameEl = document.querySelector("character-name")
+const characterClassEl = document.querySelector("character-class")
+const characterRaceEl = document.querySelector("character-race")
+const characterHpEl = document.querySelector("character-hp")
+const characterStrEl = document.querySelector("character-strength")
+const characterIntEl = document.querySelector("character-intelligence")
+const characterDexEl = document.querySelector("character-dexterity")
+const characterWisEl = document.querySelector("character-wisdom")
+const characterConEl = document.querySelector("character-constitution")
+const characterChaEl = document.querySelector("character-charisma")
+const updateBtn = document.querySelector("update-btn")
+
+const updateCharacter = async (data) => {
+    data.preventDefault()
+    
+}
+updateBtn.addEventListener("submit", updateCharacter)
