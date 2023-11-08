43431e2ec43dfc1b3f9a9b21500f93eba99be0fe
ChgDave <chgdave@gmail.com>
2023-11-08 16:13:38 -0600

    deleted validate for email for username

--- models/User.js
+++ models/User.js
@@ -23,9 +23,6 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      validate: {
-        isEmail: true,
-      },
     },
     password: {
       type: DataTypes.STRING,
