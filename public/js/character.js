6c70a6cffd22954b910b3bee5748173fa258ef11
Flunder620 <jguzman12310@yahoo.com>
2023-11-07 19:48:01 -0600

    added input fields

--- public/js/character.js
+++ public/js/character.js
@@ -3,16 +3,23 @@ const characterNameEl = document.querySelector("character-name")
 const characterClassEl = document.querySelector("character-class")
 const characterRaceEl = document.querySelector("character-race")
 const characterHpEl = document.querySelector("character-hp")
+const charHpInputEl = document.querySelector("hp-input")
 const characterStrEl = document.querySelector("character-strength")
+const charStrInputEl = document.querySelector("strength-input")
 const characterIntEl = document.querySelector("character-intelligence")
+const charIntInputEl = document.querySelector("intelligence-input")
 const characterDexEl = document.querySelector("character-dexterity")
+const charDexInputEl = document.querySelector("dexterity-input")
 const characterWisEl = document.querySelector("character-wisdom")
+const charWisInputEl = document.querySelector("wisdom-input")
 const characterConEl = document.querySelector("character-constitution")
+const charConInputEl = document.querySelector("constitution-input")
 const characterChaEl = document.querySelector("character-charisma")
+const charChaInputEl = document.querySelector("charisma-input")
 const updateBtn = document.querySelector("update-btn")
 
 const updateCharacter = async (data) => {
     data.preventDefault()
-    
+    // const response = await
 }
 updateBtn.addEventListener("submit", updateCharacter)
